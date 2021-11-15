7d9f4acce9464d0d9f51e392e6a7076614275d95
Ruth Gough <ruthegough5@gmail.com>
2021-11-14 21:14:41 -0600

    Pre-bonus exercise completed.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+//2b. Import
 
-function App() {
+import Exercise from "./exercise/Exercise";
+
+//3a-3d
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      {/* 2c */}
+      <Exercise />
+      {/* 3a */}
+      <Exercise good="Awesome" bad="Terrible" />
+    </div>);
 }
 
 export default App;
---
+++ src/exercise/Exercise.js
@@ -0,0 +1,29 @@
+//1a.  
+function Exercise ({good="Great", bad="Not Great"}){
+
+ //2a, 
+    return (
+        <div>
+        <h1>{good}</h1>
+        <h1>{bad}</h1>
+        </div>
+    );
+}
+
+/*1b. C-below: export default Exercise;
+
+2b. Added App.js file
+
+3a-3d
+
+function Exercise ({good="Great", bad="Not Great"}) {
+    return (
+        <div>
+            <h1>{good}</h1>
+            <h1>{bad}</h1>
+            </div>
+            );
+        }*/
+
+//1b
+export default Exercise
